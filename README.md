574de9d75d43839deb03018da5be1ddfd5896520
Alex Bzdel <ab913@duke.edu>
2023-02-21 01:15:58 +0000

    load in polars df

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Utilizing Rust to build an AutoML application. Will be deployed to a Kubernetes 
 ## plan going forward
 - take boiler plate code from automl crate - automate it as much as i can
     - choose from a variety of datasets? input your own data?
+    - pick dataset from smartcore options in cmd line tool format?
 - get all models back, make a function to choose the best one
     - host model on a cloud service, then predict?
     - serve predictions to some other cloud env?
+
+## end goal
+command line tool that you can pass any (cleaned) dataset into (already ohe'd, correct format, target specified, etc) and automl (only classifier for now) runs all models on it. somehow i'd like to get this deployed as a kubernetes service
--- auto/src/main.rs
+++ auto/src/main.rs
@@ -1,10 +1,45 @@
-use automl;
-use smartcore;
+// use automl;
+// use smartcore;
+use polars::prelude::*;
+
+// struct dataset_name {
+//     name: String,
+//     import_line: String,
+// }
+
+fn load_custom_dataset() -> Result<DataFrame, PolarsError>  {
+    // load in spam.csv into polars df
+    let df = CsvReader::from_path("../example_data/iris_clean.csv").unwrap().finish().unwrap();
+    println!("{}", df.head(Some(5)));
+    Ok(df)
+}
+
+/*fn get_dataset() -> dataset_name<String> {
+    // take user input to get dataset name
+    // return dataset_name
+    let mut line = String::new();
+    println!("Select dataset from the following options:");
+    let b1 = std::io::stdin().read_line(&mut line).unwrap();
+    println!("Hello , {}", line);
+
+
+}*/
 
 fn main() {
-    let dataset = smartcore::dataset::breast_cancer::load_dataset();
+    let dataset = load_custom_dataset();
+    // let dataset = smartcore::dataset::boston::load_dataset();
     let settings = automl::Settings::default_classification();
     let mut classifier = automl::SupervisedModel::new(dataset, settings);
     classifier.train();
     print!("{}", classifier);
 }
+
+
+// old code - working as intended for hardcoded dataset
+/*fn main() {
+    let dataset = smartcore::dataset::boston::load_dataset();
+    let settings = automl::Settings::default_classification();
+    let mut classifier = automl::SupervisedModel::new(dataset, settings);
+    classifier.train();
+    print!("{}", classifier);
+}*/
