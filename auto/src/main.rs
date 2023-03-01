fc4278d2646e7a9c0f17f02f7138e49b8f0c09e2
Alex Bzdel <ab913@duke.edu>
2023-03-01 18:57:54 +0000

    automl only supports two smartcore datasets - adjusted user input prompt as needed

--- auto/src/main.rs
+++ auto/src/main.rs
@@ -1,23 +1,19 @@
-use smartcore;
-use automl;
+//use smartcore;
+//use automl;
 
 fn print_dataset_names() {
-    println!("1. boston");
-    println!("2. breast_cancer");
-    println!("3. diabetes");
-    println!("4. digits");
-    println!("5. iris");
+    // print dataset names to user
+    println!("1. breast_cancer");
+    println!("2. iris");
 }
 
 fn set_dataset_name(num: i32) -> Vec<&'static str> {
     // set dataset name based on user input
-    // return dataset_name
+    // return dataset name
+    // unfortunately, these are the only smartcore datasets that work with automl at the moment
     let output = match num {
-        1 => "boston",
-        2 => "breast_cancer",
-        3 => "diabetes",
-        4 => "digits",
-        5 => "iris",
+        1 => "breast_cancer",
+        2 => "iris",
         _ => "invalid",
     };
     // return vector of selected dataset name
@@ -25,8 +21,11 @@ fn set_dataset_name(num: i32) -> Vec<&'static str> {
 
 
 }
-// create get user input function that returns an i32
+
 fn get_user_input() -> i32 {
+    // take user input
+    // return user input
+
     // create variable named line that takes user input (string)
     let mut line = String::new();
     // create variable named b1 that takes user input (int)
@@ -43,48 +42,36 @@ fn get_user_input() -> i32 {
             line.clear();
         }
     };
-    println!("b1: {}", b1);
     b1 as i32
 }
 
 fn get_dataset() -> String {
     // take user input to get dataset name
     // return dataset_name
-    let line = String::new();
     println!("Select dataset from the following options (type number):");
     print_dataset_names();
     let num = get_user_input();
+    println!("picked option {}", num);
     let dataset_name = set_dataset_name(num);
-    println!("picked option {}", line);
-    // return dataset_name
     dataset_name[0].to_string()
 }
 
 fn main() {
-    //let dataset = load_custom_dataset();
     let name = get_dataset();
     println!("dataset: {:?}", name);
     println!("running automl classifiers...");
-    /*
-    format + lint after copilot suggestion
-    */
-    //let dataset = smartcore::dataset::boston::load_dataset();
-
-    // take user input from name variable to load into smartcore dataset
+    
     let dataset = match name.as_str() {
-        "boston" => smartcore::dataset::boston::load_dataset(),
         "breast_cancer" => smartcore::dataset::breast_cancer::load_dataset(),
-        "diabetes" => smartcore::dataset::diabetes::load_dataset(),
-        "digits" => smartcore::dataset::digits::load_dataset(),
-        "iris" => smartcore::dataset::iris::load_dataset(),
-        _ => smartcore::dataset::iris::load_dataset(),
+        "iris" => smartcore::dataset::breast_cancer::load_dataset(),
+        // if user input is invalid, return error
+        _ => panic!("invalid dataset name"),
     };
-
     // pass in dataset to automl classifier
-
     let settings = automl::Settings::default_classification();
     // make classifier variable that uses the dataset and the settings variables above
     let mut classifier = automl::SupervisedModel::new(dataset, settings);
     classifier.train();
+    println!("makes it past train");
     print!("{}", classifier);
 }
